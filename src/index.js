10ad75d6bfe29fc6b598eb187eb42e704faf3730
haizlin <80285586@qq.com>
2019-05-29 05:37:16 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const images = require('images');
+const imageInfo = require("imageinfo");
+const xlsx = require('node-xlsx');
+
+// 读取文件夹下的图片
+function readPhotoList(path){
+
+}
+
+// 读取excel文件
+function readExcel(path){
+
+}
+
+// 开始生成水印
+function generateWatermark(){
+
+}
